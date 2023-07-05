07ca6209fcd347c50cde7ad176d2cce12e92088f
Insticthere <omgnitingupta@gmail.com>
2023-07-05 18:28:30 +0530

    i

--- src/images/upload.js
+++ src/images/upload.js
@@ -39,7 +39,7 @@ export default function uploadImage(app) {
     await drive.put(`${id}.${removedPart}`, { data: view });
     return c.json({
       id : id,
-      url : `${new URL(c.req.url).protocol}//${c.req.headers.get('host')}/u/get/${id}`,
+      url : `${new URL(c.req.url).protocol}//${c.req.headers.get('host')}/i/get/${id}`,
       image: image.name
     }, 200);
      
